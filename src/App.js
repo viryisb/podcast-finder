033400a87ef39396f2659a789ae071f054d79064
Virginia <bernasconivir@gmail.com>
2023-03-02 17:14:58 -0300

    add route to home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import PodcastListContainer from './components/PodcastList/PodcastListContainer';
+import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className='App'>Poadcast finder</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<PodcastListContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
