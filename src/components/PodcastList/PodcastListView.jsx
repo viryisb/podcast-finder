bb3211db710e90b74815f21c79e1533405d01974
Virginia <bernasconivir@gmail.com>
2023-03-02 22:14:42 -0300

    split components & style podcast list

--- src/components/PodcastList/PodcastListView.jsx
+++ src/components/PodcastList/PodcastListView.jsx
@@ -1,13 +1,17 @@
-import Podcast from '../Podcast/Podcast';
+import Podcast from '../podcast/Podcast';
 import SearchBar from '../searchBar/SearchBar';
+
+import styles from './PodcastListView.module.css';
 
 export default function PodcastListView({ podcasts }) {
   return (
-    <>
+    <div className={styles.container}>
       <SearchBar />
-      {podcasts.map((podcast) => (
-        <Podcast key={podcast.id.attributes['im:id']} podcast={podcast} />
-      ))}
-    </>
+      <div className={styles.cards}>
+        {podcasts.map((podcast) => (
+          <Podcast key={podcast.id.attributes['im:id']} podcast={podcast} />
+        ))}
+      </div>
+    </div>
   );
 }
