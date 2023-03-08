9d807a67c9c931fd598d6295839c688e7833d02c
Virginia <bernasconivir@gmail.com>
2023-03-07 23:57:06 -0300

    add context for spinner

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import PodcastListContainer from './components/podcastList/PodcastListContainer'
 import PodcastDetailContainer from './components/podcastDetail/podcastDetailContainer';
 import Header from './components/header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoadingProvider } from './components/context/LoadingContext';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
+      <LoadingProvider>
+        <BrowserRouter>
+          <Header />
 
-        <Routes>
-          <Route path='/' element={<PodcastListContainer />} />
-          <Route path='/podcast/:id' element={<PodcastDetailContainer />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PodcastListContainer />} />
+            <Route path='/podcast/:id' element={<PodcastDetailContainer />} />
+          </Routes>
+        </BrowserRouter>
+      </LoadingProvider>
     </div>
   );
 }
