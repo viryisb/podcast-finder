b800f2a202770b868d76656e58cbde09a54e4cdc
Virginia <bernasconivir@gmail.com>
2023-03-12 00:22:53 -0300

    fix styling

--- src/components/episodeDetail/EpisodeDetailView.jsx
+++ src/components/episodeDetail/EpisodeDetailView.jsx
@@ -1,16 +1,15 @@
-import PodcastDetailBar from '../podcastDetailBar/PodcastDetailBar';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import styles from './EpisodeDetailView.module.css';
 
 export default function EpisodeDetailView({ episode }) {
   return (
-    <div>
-      {episode.title && <p>{episode.title}</p>}
+    <div className={styles.episodeDetailContainer}>
+      {episode.title && <p className={styles.title}>{episode.title}</p>}
       {episode.description && (
         <div dangerouslySetInnerHTML={{ __html: episode.description }} />
       )}
-      {episode.audio && <audio controls src={episode.audio} />}
+      {episode.audio && (
+        <audio controls src={episode.audio} className={styles.audioPlayer} />
+      )}
     </div>
   );
 }
